3305b281785a0969791457d4a09db826d92d9c2b
maddes <maddes@98b46aec-ca4e-497a-8d32-a79c971cecba>
2011-03-22 00:23:12 +0000

    Move build stuff from root folder to build folder

--- trunk/src/setup.c
+++ trunk/src/setup.c
@@ -157,7 +157,7 @@ BOOL IsInSkipList(LPSTR lpSnap, LPDWORD lpSkipList) //tfx Ìø¹ýºÚÃûµ¥
 {
 	int i;
 	for(i=0; (LPSTR)(*(lpSkipList+i))!=NULL && i<=MAXREGSHOT-1; i++) {
-		if(_strcmpi(lpSnap, (LPSTR)*(lpSkipList+i))==0) {
+		if(_stricmp(lpSnap, (LPSTR)*(lpSkipList+i))==0) {
 			return TRUE;
 		}
 	}
