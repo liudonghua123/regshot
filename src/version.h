2bd9d4292aa37bddbb3ffec9006a7252af264008
maddes <maddes@98b46aec-ca4e-497a-8d32-a79c971cecba>
2012-12-26 03:21:45 +0000

    Add further MSC versions

--- src/version.h
+++ src/version.h
@@ -40,21 +40,35 @@
 #       define REGSHOT_VERSION_COMPILER TEXT("Intel Compiler (version unknown)")
 #   endif
 #elif defined(WDK_BUILD)
-#   if _MSC_VER == 1600
+#   if _MSC_VER == 1700
+#       define REGSHOT_VERSION_COMPILER TEXT("WDK (MSVC 2012 SP0)")
+#   elif _MSC_VER == 1600
 #       if (_MSC_FULL_VER >= 160040219)
 #           define REGSHOT_VERSION_COMPILER TEXT("WDK (MSVC 2010 SP1)")
 #       else
 #           define REGSHOT_VERSION_COMPILER TEXT("WDK (MSVC 2010 SP0)")
 #       endif
 #   elif _MSC_VER == 1500
-#       if (_MSC_FULL_VER == 150030729)
-#           define REGSHOT_VERSION_COMPILER TEXT("WDK")
+#       if (_MSC_FULL_VER >= 150030729)
+#           define REGSHOT_VERSION_COMPILER TEXT("WDK (MSVC 2008 SP1)")
 #       else
-#           define REGSHOT_VERSION_COMPILER TEXT("WDK (version unknown)")
+#           define REGSHOT_VERSION_COMPILER TEXT("WDK (MSVC 2008 SP0)")
 #       endif
+#   elif _MSC_VER == 1400
+#       define REGSHOT_VERSION_COMPILER TEXT("WDK (MSVC 2005)")
+#   elif _MSC_VER == 1310
+#       define REGSHOT_VERSION_COMPILER TEXT("WDK (MSVC 2003)")
+#   elif _MSC_VER == 1300
+#       define REGSHOT_VERSION_COMPILER TEXT("WDK (MSVC 2002)")
+#   elif _MSC_VER == 1200
+#       define REGSHOT_VERSION_COMPILER TEXT("WDK (MSVC 6.0)")
+#   else
+#       define REGSHOT_VERSION_COMPILER TEXT("WDK (version unknown)")
 #   endif
 #elif defined(_MSC_VER)
-#   if _MSC_VER == 1600
+#   if _MSC_VER == 1700
+#       define REGSHOT_VERSION_COMPILER TEXT("MSVC 2012 SP0")
+#   elif _MSC_VER == 1600
 #       if (_MSC_FULL_VER >= 160040219)
 #           define REGSHOT_VERSION_COMPILER TEXT("MSVC 2010 SP1")
 #       else
@@ -66,12 +80,19 @@
 #       else
 #           define REGSHOT_VERSION_COMPILER TEXT("MSVC 2008 SP0")
 #       endif
+#   elif _MSC_VER == 1400
+#       define REGSHOT_VERSION_COMPILER TEXT("MSVC 2005")
+#   elif _MSC_VER == 1310
+#       define REGSHOT_VERSION_COMPILER TEXT("MSVC 2003")
+#   elif _MSC_VER == 1300
+#       define REGSHOT_VERSION_COMPILER TEXT("MSVC 2002")
+#   elif _MSC_VER == 1200
+#       define REGSHOT_VERSION_COMPILER TEXT("MSVC 6.0")
 #   else
 #       define REGSHOT_VERSION_COMPILER TEXT("MSVC (version unknown)")
 #   endif
 #else
 #   define REGSHOT_VERSION_COMPILER TEXT("(Unknown compiler)")
 #endif
-
 
 #endif // REGSHOT_VERSION_H
