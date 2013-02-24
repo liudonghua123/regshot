5ad43bd65b2f69bb81a9c2580488696e6e85b8e2
maddes <maddes@98b46aec-ca4e-497a-8d32-a79c971cecba>
2013-02-24 14:11:54 +0000

    Add QWORD reference Fix big endian values

--- tests/regytpes.bat
+++ tests/regytpes.bat
@@ -1,9 +1,13 @@
 @rem *** Empty values
 reg add HKCU\RegTypes /v REG_NONE_EMPTY /t REG_NONE /f
 reg add HKCU\RegTypes /v REG_BINARY_EMPTY /t REG_BINARY /f
-reg add HKCU\RegTypes /v REG_DWORD_EMPTY /t REG_DWORD /f
-reg add HKCU\RegTypes /v REG_DWORD_LITTLE_ENDIAN_EMPTY /t REG_DWORD_LITTLE_ENDIAN /f
-reg add HKCU\RegTypes /v REG_DWORD_BIG_ENDIAN_EMPTY /t REG_DWORD_BIG_ENDIAN /f
+reg add HKCU\RegTypes /v REG_DWORD_EMPTY /t REG_DWORD /f /d 0
+reg add HKCU\RegTypes /v REG_DWORD_LITTLE_ENDIAN_EMPTY /t REG_DWORD_LITTLE_ENDIAN /f /d 0
+reg add HKCU\RegTypes /v REG_DWORD_BIG_ENDIAN_EMPTY /t REG_DWORD_BIG_ENDIAN /f /d 0
+reg add HKCU\RegTypes /v REG_QWORD_EMPTY /t REG_QWORD /f /d 0
+reg add HKCU\RegTypes /v REG_QWORD_LITTLE_ENDIAN_EMPTY /t REG_QWORD_LITTLE_ENDIAN /f /d 0
+reg add HKCU\RegTypes /v REG_QWORD_BIG_ENDIAN_EMPTY /t REG_QWORD_BIG_ENDIAN /f /d 0
+
 reg add HKCU\RegTypes /v REG_SZ_EMPTY /t REG_SZ /f
 reg add HKCU\RegTypes /v REG_EXPAND_SZ_EMPTY /t REG_EXPAND_SZ /f
 reg add HKCU\RegTypes /v REG_MULTI_SZ_EMPTY /t REG_MULTI_SZ /f
@@ -13,7 +17,11 @@ reg add HKCU\RegTypes /v REG_NONE /t REG_NONE /f /d FakeString
 reg add HKCU\RegTypes /v REG_BINARY /t REG_BINARY /f /d 12345678
 reg add HKCU\RegTypes /v REG_DWORD /t REG_DWORD /f /d 0x12345678
 reg add HKCU\RegTypes /v REG_DWORD_LITTLE_ENDIAN /t REG_DWORD_LITTLE_ENDIAN /f /d 0x12345678
-reg add HKCU\RegTypes /v REG_DWORD_BIG_ENDIAN /t REG_DWORD_BIG_ENDIAN /f /d 0x12345678
+reg add HKCU\RegTypes /v REG_DWORD_BIG_ENDIAN /t REG_DWORD_BIG_ENDIAN /f /d 0x78563412
+reg add HKCU\RegTypes /v REG_QWORD /t REG_QWORD /f /d 123456789ABCDEF0
+reg add HKCU\RegTypes /v REG_QWORD_LITTLE_ENDIAN /t REG_QWORD_LITTLE_ENDIAN /f /d 123456789ABCDEF0
+reg add HKCU\RegTypes /v REG_QWORD_BIG_ENDIAN /t REG_QWORD_BIG_ENDIAN /f /d F0DEBC9A78563412
+
 reg add HKCU\RegTypes /v REG_SZ /t REG_SZ /f /d String
 reg add HKCU\RegTypes /v REG_EXPAND_SZ /t REG_EXPAND_SZ /f /d String
 reg add HKCU\RegTypes /v REG_MULTI_SZ /t REG_MULTI_SZ /f /d String1\0String2\0String3
